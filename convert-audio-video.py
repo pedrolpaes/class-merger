845011a92f85f08dc0a4415c753522d82b8af9d4
Pedro Leitão Paes <47438581+pedrolpaes@users.noreply.github.com>
2021-05-19 04:49:06 -0300

    Add files via upload

--- convert-audio-video.py
+++ convert-audio-video.py
@@ -6,11 +6,10 @@ for dirpath, dirnames, filenames in os.walk(r'D:\Faculdade\Aulas'):
            inputfilepath = os.path.join(dirpath, i)
            outputfilepath = dirpath + '/audio.aac' 
            subprocess.call(['ffmpeg', '-i', inputfilepath, '-vn', outputfilepath])
+           inputvideopath = dirpath + '/video.mp4'
+           inputaudiopath = dirpath + '/audio.aac'
+           outputfilepath = dirpath + '/video_completo.mp4'
+           subprocess.call(['ffmpeg', '-i', inputvideopath, '-i', inputaudiopath, outputfilepath])
+           os.remove(inputaudiopath)
         
-    for i in filenames:
-        if(not os.path.isfile(dirpath + '/video_completo.mp4') and (i.lower()=="video.mp4")):
-            inputvideopath = os.path.join(dirpath,i)
-            inputaudiopath = dirpath + '/audio.aac'
-            outputfilepath = dirpath + '/video_completo.mp4'
-            subprocess.call(['ffmpeg', '-i', inputvideopath, '-i', inputaudiopath, outputfilepath])
-            os.remove(inputaudiopath)
+  
